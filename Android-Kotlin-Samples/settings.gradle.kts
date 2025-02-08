49328d813b56d67ab48b16de9227bbc4e6c4d0c6
Venkat Ballem <>
2025-02-08 11:04:05 -0600

    added design-patterns lib

--- Android-Kotlin-Samples/settings.gradle.kts
+++ Android-Kotlin-Samples/settings.gradle.kts
@@ -17,4 +17,5 @@ rootProject.name = "Samples - reified.dev"
 
 include(":android-apps:template-app-module")
 include(":kotlin-samples:template-module")
+include(":kotlin-samples:ds-alg")
 include(":design-patterns")
